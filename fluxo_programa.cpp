f74aadd8833caddc5536bda0acc3368224904673
Filipe <lipecs.1111@gmail.com>
2026-01-07 14:26:26 -0300

    Add functions to order a list and find a number in the list

--- fluxo_programa.cpp
+++ fluxo_programa.cpp
@@ -65,6 +65,7 @@ Good luck!
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -75,6 +76,8 @@ void add_num(vector <int> &l);
 void mean_num(vector <int> l);
 void smallest_num(vector <int> l);
 void largest_num(vector <int> l);
+void order_list(vector <int> &l);
+void find_number(vector <int> l);
 
 int main() {
     
@@ -112,7 +115,7 @@ int main() {
             break;
 
         case 'L':
-             if(lista_vazia(lista) == false){
+            if(lista_vazia(lista) == false){
                 largest_num(lista);
             }    
             break;
@@ -120,7 +123,19 @@ int main() {
         case 'Q':   
             cout << "Goodbye" << "\n";
             break;  
-        
+
+        case 'O':
+            if(lista_vazia(lista) == false){
+                order_list(lista);
+            }  
+            break;
+
+        case 'F':
+            if(lista_vazia(lista) == false){
+                find_number(lista);
+            }  
+            break;
+            
         default:
             cout << "Unknown selection, please try again" << "\n";
             break;
@@ -138,6 +153,8 @@ void menu() {
     cout << "M - Display mean of the numbers" << "\n";
     cout << "S - Display the smallest number" << "\n";
     cout << "L - Display the largest number" << "\n";
+    cout << "O - Order the list" << "\n";
+    cout << "F - Find a number in the list" << "\n";
     cout << "Q - Quit" << "\n";
     cout << "\nEnter your choice:" << "\n";
 }
@@ -201,3 +218,169 @@ void largest_num(vector <int> l){
     cout << "The biggest number is " << maior << "\n";
 }       
 
+void order_list(vector <int> &l){
+    sort(l.begin(), l.end());
+
+    cout << "The list has been sorted" << "\n";
+}
+
+void find_number(vector <int> l) {
+    int num;
+    bool found = false;
+    cout << "Enter the number: " << "\n";
+    cin >> num;
+
+    for(size_t i{0}; i < l.size() ; ++i){
+        if(l.at(i) == num){
+            cout << "Number " << num << " was found at the position "; 
+            cout << i + 1 << " of the list" << "\n";
+            found = true;
+        } 
+    }
+
+    if(found == false) {
+            cout << "Number " << num << "was not found at the list" << "\n";
+    }
+}
+
+/*
+Código refatorado por gemini para estudo pessoal.
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // Para sort, min_element, max_element
+#include <numeric>   // Para accumulate (soma de vetores)
+
+using namespace std;
+
+// Protótipos das funções
+void exibir_menu();
+void imprimir_lista(const vector<int> &v); // Passagem por referência constante
+void adicionar_numero(vector<int> &v);
+void exibir_media(const vector<int> &v);
+void exibir_menor(const vector<int> &v);
+void exibir_maior(const vector<int> &v);
+void ordenar_lista(vector<int> &v);
+void buscar_numero(const vector<int> &v);
+
+int main() {
+    vector<int> lista;
+    char escolha;
+
+    do {
+        exibir_menu();
+        cin >> escolha;
+        escolha = toupper(escolha);
+
+        switch (escolha) {
+            case 'P': imprimir_lista(lista); break;
+            case 'A': adicionar_numero(lista); break;
+            case 'M': exibir_media(lista); break;
+            case 'S': exibir_menor(lista); break;
+            case 'L': exibir_maior(lista); break;
+            case 'O': ordenar_lista(lista); break;
+            case 'F': buscar_numero(lista); break;
+            case 'Q': cout << "Goodbye!" << endl; break;
+            default:  cout << "Unknown selection, please try again" << endl;
+        }
+    } while (escolha != 'Q');
+
+    return 0;
+}
+
+void exibir_menu() {
+    cout << "\n--- Opcoes ---" << "\n"
+         << "P - Print numbers\n"
+         << "A - Add a number\n"
+         << "M - Display mean\n"
+         << "S - Display smallest\n"
+         << "L - Display largest\n"
+         << "O - Order list\n"
+         << "F - Find a number\n"
+         << "Q - Quit\n"
+         << "\nEnter choice: ";
+}
+
+// 1. Passagem por const vector<int> &v: Não copia o vetor e garante que não será alterado
+void imprimir_lista(const vector<int> &v) {
+    if (v.empty()) {
+        cout << "[] - the list is empty" << endl;
+        return;
+    }
+    cout << "[ ";
+    for (const auto &num : v) { // 2. Range-based for loop: mais limpo e moderno
+        cout << num << " ";
+    }
+    cout << "]" << endl;
+}
+
+void adicionar_numero(vector<int> &v) {
+    int num;
+    cout << "Enter an integer to add: ";
+    cin >> num;
+    v.push_back(num);
+    cout << num << " added" << endl;
+}
+
+void exibir_media(const vector<int> &v) {
+    if (v.empty()) {
+        cout << "Unable to calculate the mean - no data" << endl;
+        return;
+    }
+    // 3. std::accumulate: Forma idiomática de somar valores em C++
+    double soma = accumulate(v.begin(), v.end(), 0.0);
+    cout << "The mean is: " << soma / v.size() << endl;
+}
+
+void exibir_menor(const vector<int> &v) {
+    if (v.empty()) {
+        cout << "Unable to determine the smallest number - list is empty" << endl;
+        return;
+    }
+    // 4. std::min_element: Retorna um iterador para o menor elemento
+    auto menor = min_element(v.begin(), v.end());
+    cout << "The smallest number is " << *menor << endl;
+}
+
+void exibir_maior(const vector<int> &v) {
+    if (v.empty()) {
+        cout << "Unable to determine the largest number - list is empty" << endl;
+        return;
+    }
+    auto maior = max_element(v.begin(), v.end());
+    cout << "The largest number is " << *maior << endl;
+}
+
+void ordenar_lista(vector<int> &v) {
+    if (v.empty()) {
+        cout << "List is empty, nothing to sort." << endl;
+        return;
+    }
+    sort(v.begin(), v.end());
+    cout << "The list has been sorted" << endl;
+}
+
+void buscar_numero(const vector<int> &v) {
+    if (v.empty()) {
+        cout << "List is empty." << endl;
+        return;
+    }
+    int alvo;
+    cout << "Enter the number to find: ";
+    cin >> alvo;
+
+    int contador = 0;
+    for (size_t i = 0; i < v.size(); ++i) {
+        if (v[i] == alvo) {
+            cout << "Found " << alvo << " at index " << i << endl;
+            contador++;
+        }
+    }
+
+    if (contador == 0) 
+        cout << alvo << " was not found" << endl;
+    else 
+        cout << "Total occurrences: " << contador << endl;
+}
+
+*/
