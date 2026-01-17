adebbc67ec0f4c816b04e9ca0346767ad59cbeab
Filipe <lipecs.1111@gmail.com>
2026-01-17 11:36:05 -0300

    Refactor Player class to include talk method implementation and streamline enemy creation in main function

--- classes_objt.cpp
+++ classes_objt.cpp
@@ -3,17 +3,17 @@
 #include <vector>
 
 class Player {
+public:
     std::string name;
     int health;
     int xp;
 
-    void talk(std::string);
+    void talk(std::string texto_to_say) { std::cout << name << " says " << texto_to_say << "\n"; }
     bool is_dead();
 };
 
 int main() {
     Player hero;
-    Player enemy;
 
     /*
     -- É possivel criar array como qualquer outro tipo
@@ -29,6 +29,26 @@ int main() {
     std::vector <Player> player_vec {hero};
     player_vec.push_back(hero);
     */
+
+    hero.name = "Filipe";
+    hero.xp = 2;
+    hero.health = 100;
+
+    hero.talk("Hello you");
+
+    std::cout << "My name is " << hero.name;
+    std::cout << " and my current xp and healh is " << hero.xp << "/" << hero.health << "\n";
+
+    std::cout << "Enemy is arriving...\n";
+
+    //enemy foi criado na heap, ponteiro recebeu endereço, por ser ponteiro usa-se -> ou dereferência * e utiliza .   
+    Player *enemy = new Player;
+    enemy->name = "Enemy";
+    //ou
+    (*enemy).talk("I will destroy you!");
+    (*enemy).health = 100;
+
+    delete enemy;
 
     return 0;
 }
