6668c6a9fd290dc1fd74151c22ba78090590b70d
Filipe <lipecs.1111@gmail.com>
2026-01-28 22:40:27 -0300

    Refactor Lutador class to include healing functionality and potion management

--- console_game.cpp
+++ console_game.cpp
@@ -9,22 +9,25 @@ private:
     size_t vida;
     size_t dano;
     size_t vida_maxima; 
+    size_t pocao_vida;
 public:
-    Lutador(std::string n, size_t v, size_t d, size_t vm = 0);
+    Lutador(std::string n, size_t v, size_t d, size_t vm = 0, size_t pv = 0);
 
     std::string getNome() const;
     size_t getVida() const;
     size_t getDano() const;
     size_t getVidaMaxima() const;
+    size_t getPocaoVida() const;
 
     void atacar(Lutador &l);
-    void curar(Lutador &l);
+    void curar();
 };
 
 std::string Lutador::getNome() const { return nome; }
 size_t Lutador::getVida() const { return vida; }
 size_t Lutador::getDano() const { return dano; }
 size_t Lutador::getVidaMaxima() const { return vida_maxima; }
+size_t Lutador::getPocaoVida() const { return pocao_vida; }
 
 void Lutador::atacar(Lutador &l) {
     size_t dano_causado = (rand() % dano) + 1;
@@ -38,30 +41,39 @@ void Lutador::atacar(Lutador &l) {
     std::cout << nome << " atacou " << l.nome << " causando " << dano_causado << " de dano.\n";
 }
 
-void Lutador::curar(Lutador &l) {
+void Lutador::curar() {
     if(vida == vida_maxima){
         std::cout << nome << " ja esta com vida maxima" << std::endl;
         return;
     } 
 
-    size_t cura = (rand() % 30) + 10;
-
-    if (l.vida + cura > l.vida_maxima) {
-        l.vida = l.vida_maxima;
-    } else {
-        l.vida += cura;
+    if(pocao_vida == 0){
+        std::cout << "As pocoes de cura acabaram" << std::endl;
+        return;
     }
 
-    std::cout << nome << " curou " << l.nome << " em " << cura << " pontos de vida.\n";
+    size_t cura = (rand() % 30) + 10;
+
+    if (vida + cura > vida_maxima) {
+        vida = vida_maxima;
+    } else {
+        vida += cura;
+    }
+
+    std::cout << "Curando..." << std::endl;
+    std::cout << nome << " curou " << nome << " em " << cura << " pontos de vida.\n";
+    pocao_vida -= 1;
+    std::cout << "Restam " << pocao_vida << " pocoes" << std::endl; 
+    
 }
 
 //list initializer for constructor
-Lutador::Lutador(std::string n, size_t v, size_t d, size_t vm)
-    : nome(n), vida(v), dano(d), vida_maxima(vm) {}
+Lutador::Lutador(std::string n, size_t v, size_t d, size_t vm, size_t pv)
+    : nome(n), vida(v), dano(d), vida_maxima(vm), pocao_vida(pv) {}
 
 int main() {
     srand(time(0));
-    Lutador heroi("Filipe", 125, 50, 125);
+    Lutador heroi("Filipe", 125, 50, 125, 3);
     Lutador vilao("Vilao", 150, 50, 150);
 
     while(heroi.getVida() > 0 && vilao.getVida() > 0) {
@@ -82,7 +94,7 @@ int main() {
         switch (escolha)
         {
         case 1: heroi.atacar(vilao); break;
-        case 2: heroi.curar(heroi); break;
+        case 2: heroi.curar(); break;
         default: std::cout << "Digite uma opcao valida" << std::endl; break;   
         }
 
@@ -91,9 +103,11 @@ int main() {
             break; 
         }
 
-        std::cout << "\nO inimigo se prepara para atacar...\n";
-        vilao.atacar(heroi); 
-
+        if(escolha == 1){
+            std::cout << "\nO inimigo se prepara para atacar...\n";
+            vilao.atacar(heroi); 
+        }
+        
         if(heroi.getVida() == 0) {
             std::cout << "\nVOCE MORREU!\n";
             break;
@@ -104,4 +118,4 @@ int main() {
     }
 
     return 0;
-}
+} 
